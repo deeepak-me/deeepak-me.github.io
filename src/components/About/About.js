0abd954b68bf14b7dddc6623823ad44f4d7c3eb0
deeepak-me <deepak.krishnan.e@gmail.com>
2024-04-17 23:45:14 +0530

    feat: content changed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React from "react";
 import "./About.css";
-import { Info } from './Info';
+import { Info } from "./Info";
 
 export const About = () => {
   return (
     <section className="about section" id="about">
-        <h2 className="section__title">About Me</h2>
-        <span className="section__subtitle">My Introduction</span>
+      <h2 className="section__title">About Me</h2>
+      <span className="section__subtitle">My Introduction</span>
 
-        <div className="about__container container grid">
+      <div className="about__container container grid">
+        <div className="about__data">
+          <p className="about__description">
+            Hey there, I'm Deepak, a simple guy from Kerala! 🌴 who loves to
+            develop cool website and call himself a front end developer 🤓💻 .
+            I'm that guy who drinks tea( chai ) like it's a renewable resource
+            and adore using codes to turn ideas into reality.{" "}
+            <br>
+              When I'm not making websites look cooler than a polar bear in
+              sunglasses, you'll find me binge-watching movies, screaming at
+              football matches( forever a Madridista ), and chatting about all
+              kinds of random stuff with my favorite peeps!🚀
+            </br>{" "}
+            Let's sprinkle some frontend fairy dust together and create web
+            wonders that'll make users go, "Whoa, that's slick!" 😄✨
+          </p>
 
-          <div className="about__data">
-            <p className="about__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti modi voluptatem qui voluptate non dignissimos assumenda amet vel autem nulla cumque vitae beatae, inventore fuga facere harum sunt eveniet suscipit?</p>
+          <Info />
 
-            <Info />
-
-            <a download="" href="" className="button button--flex">Download CV</a>
-          </div>
-
+          <a download="" href="" className="button button--flex">
+            Download CV
+          </a>
         </div>
+      </div>
     </section>
-  )
-}
-
+  );
+};
